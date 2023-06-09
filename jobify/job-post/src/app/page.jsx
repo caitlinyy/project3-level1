ce2773702fa0eccb90d77f2e1302f30db6858c32
caitlinyy <caitlinyy@outlook.com>
2023-06-09 13:00:53 +1000

    initial styled-components

--- jobify/job-post/src/app/page.jsx
+++ jobify/job-post/src/app/page.jsx
@@ -1,11 +1,19 @@
-'use client';
+"use client";
 
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import styled from "styled-components";
+
+// const Test = styled.div`
+//   width: 500px;
+//   height: 500px;
+//   background-color: yellow;
+// `;
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      {/* <Test /> */}
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
@@ -17,28 +25,14 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By{" "}
+            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
       </div>
 
       <div className={styles.grid}>
@@ -87,11 +81,9 @@ export default function Home() {
           <h2>
             Deploy <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
-  )
+  );
 }
